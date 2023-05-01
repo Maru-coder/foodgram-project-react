47621e372fe60e512418b8692ed9cce7c0c20365
Марина Ежова <marickyarick@gmail.com>
2023-05-01 07:08:08 +0600

    modified settings

---
+++ backend/recipes/utils/load_data_ingr.py
@@ -0,0 +1,26 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram.settings')
+import django
+django.setup()
+
+import csv
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Загрузка из csv файла'
+
+    def handle(self, *args, **kwargs):
+        data_path = settings.BASE_DIR
+        with open(
+                f'{data_path}/data/ingredients.csv',
+                'r',
+                encoding='utf-8'
+        ) as file:
+            reader = csv.DictReader(file)
+            Ingredient.objects.bulk_create(
+                Ingredient(**data) for data in reader)
+        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
